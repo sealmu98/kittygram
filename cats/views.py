cba0cf10634eebf7ebbacd6c945dcf8c6851dcfc
sealmu <seal.mu98@gmail.com>
2024-09-16 00:15:13 +0300

    GET, POST

--- cats/views.py
+++ cats/views.py
@@ -8,12 +8,24 @@ from .serializers import CatSerializer
 
 @api_view(['GET', 'POST'])
 def cat_list(request):
+    # В случае POST-запроса добавим список записей в БД
     if request.method == 'POST':
+        # Создаём объект сериализатора
+        # и передаём в него данные из POST-запроса
         serializer = CatSerializer(data=request.data)
         if serializer.is_valid():
+            # Если полученные данные валидны —
+            # сохраняем данные в базу через save().
             serializer.save()
+            # Возвращаем JSON со всеми данными нового объекта
+            # и статус-код 201
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # Если данные не прошли валидацию —
+        # возвращаем информацию об ошибках и соответствующий статус-код:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # В случае GET-запроса возвращаем список всех котиков
+    # Получаем все объекты модели
     cats = Cat.objects.all()
+    # Передаём queryset в конструктор сериализатора
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from cats.views import cat_list
 
 urlpatterns = [
-   path('cats/', cat_list),
+    path('cats/', cat_list),
 ]
-
-
