da012ed6544fbd9026eeeecd801f616d7b16f2ac
sealmu <seal.mu98@gmail.com>
2024-09-19 00:00:45 +0300

    ViewSet

---
+++ cats/views_g.py
@@ -0,0 +1,14 @@
+from rest_framework import generics
+
+from .models import Cat
+from .serializers import CatSerializer
+
+
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
---
+++ kittygram/urls_g.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from cats.views import CatList, CatDetail
+
+urlpatterns = [
+    path('cats/', CatList.as_view()),
+    path('cats/<int:pk>/', CatDetail.as_view()),
+]
