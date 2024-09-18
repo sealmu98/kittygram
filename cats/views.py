1efb66bdc8d0f13f874017c4d327610b9a1ca48d
sealmu <seal.mu98@gmail.com>
2024-09-18 20:37:24 +0300

    cat_detail

--- cats/views.py
+++ cats/views.py
@@ -29,3 +29,33 @@ def cat_list(request):
     # Передаём queryset в конструктор сериализатора
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def cat_detail(request, pk):
+    # Получаем объект Cat по pk
+    cat = Cat.objects.get(pk=pk)
+
+    # В случае PUT или PATCH-запроса обновляем объект Cat
+    if request.method == 'PUT' or request.method == 'PATCH':
+        # Создаём объект сериализатора с данными из запроса
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            # Если полученные данные валидны — сохраняем изменения
+            serializer.save()
+            # Возвращаем JSON со всеми данными обновлённого объекта
+            # и статус-код 200
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        # Если данные не прошли валидацию —
+        # возвращаем информацию об ошибках и соответствующий статус-код:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # В случае DELETE-запроса удаляем объект Cat
+    elif request.method == 'DELETE':
+        cat.delete()
+        # Возвращаем пустой ответ с статус-кодом 204
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # В случае GET-запроса возвращаем данные объекта Cat
+    serializer = CatSerializer(cat)
+    return Response(serializer.data)
